de64ccea243ff3cb340da6b882fe76594bec0614
Daniel Hahn <dha@betterplace.org>
2012-07-31 17:16:34 +0200

    Updated version to use with default sti naming setup for Rails >= 2.3

--- lib/acts_as_account/posting.rb
+++ lib/acts_as_account/posting.rb
@@ -2,9 +2,9 @@ module ActsAsAccount
   class Posting < ActiveRecord::Base
     set_table_name :acts_as_account_postings
     
-    belongs_to :account
-    belongs_to :other_account, :class_name => 'Account'
-    belongs_to :journal
+    belongs_to :account, :class_name => 'ActsAsAccount::Account'
+    belongs_to :other_account, :class_name => 'ActsAsAccount::Account'
+    belongs_to :journal, :class_name => 'ActsAsAccount::Journal'
     belongs_to :reference, :polymorphic => true
     
     named_scope :soll,  :conditions => 'amount >= 0'
