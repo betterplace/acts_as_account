57d67120f166ef7cce4fdbd853343b10b97bfab6
Meriton Chutra <tony@mypizza.com>
2015-11-16 15:17:44 +0100

    Added description to the gem

--- lib/acts_as_account/journal.rb
+++ lib/acts_as_account/journal.rb
@@ -25,33 +25,34 @@ module ActsAsAccount
       end
     end
 
-    def transfer(amount, from_account, to_account, reference = nil, valuta = Time.now)
+    def transfer(amount, from_account, to_account, reference = nil, valuta = Time.now, description = nil)
       transaction do
         if (amount < 0)
           # change order if amount is negative
           amount, from_account, to_account = -amount, to_account, from_account
         end
 
-        logger.debug { "ActsAsAccount::Journal.transfer amount: #{amount} from:#{from_account.id} to:#{to_account.id} reference:#{reference.class.name}(#{reference.id}) valuta:#{valuta}" } if logger
+        logger.debug { "ActsAsAccount::Journal.transfer amount: #{amount} from:#{from_account.id} to:#{to_account.id} reference:#{reference.class.name}(#{reference.id}) valuta:#{valuta} description:#{description}" } if logger
 
         # to avoid possible deadlocks we need to ensure that the locking order is always
         # the same therfore the sort by id.
         [from_account, to_account].sort_by(&:id).map(&:lock!)
 
-        add_posting(-amount,  from_account,   to_account, reference, valuta)
-        add_posting( amount,    to_account, from_account, reference, valuta)
+        add_posting(-amount,  from_account,   to_account, reference, valuta, description)
+        add_posting( amount,    to_account, from_account, reference, valuta, description)
       end
     end
 
     private
 
-      def add_posting(amount, account, other_account, reference, valuta)
+      def add_posting(amount, account, other_account, reference, valuta, description)
         posting = postings.build(
           :amount => amount,
           :account => account,
           :other_account => other_account,
           :reference => reference,
-          :valuta => valuta)
+          :valuta => valuta,
+          :description => description)
 
         account.class.update_counters account.id, :postings_count => 1, :balance => posting.amount
 
