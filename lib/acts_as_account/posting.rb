40aa3f3e9f486bc98f3219768f89eaefc26666f2
David Ratajczak <david@mockra.com>
2016-05-04 12:33:11 -0700

    adds for_reference scope to postings

--- lib/acts_as_account/posting.rb
+++ lib/acts_as_account/posting.rb
@@ -9,6 +9,7 @@ module ActsAsAccount
     belongs_to :journal,       :class_name => 'ActsAsAccount::Journal'
     belongs_to :reference, :polymorphic => true
 
+    scope :for_reference, -> (ref_id) { where(reference_id: ref_id) }
     scope :soll,       -> { where('amount >= 0') }
     scope :haben,      -> { where('amount < 0') }
     scope :start_date, -> date {
