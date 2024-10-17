5b0bb51f28c275f8589ddd5f22677cfbd2058ba2
Felix Wolfsteller <felix.wolfsteller@betterplace.org>
2024-10-17 11:21:32 +0200

    added top level doc for GlobalAccount

--- lib/acts_as_account/global_account.rb
+++ lib/acts_as_account/global_account.rb
@@ -1,4 +1,9 @@
 module ActsAsAccount
+  # A named account that is "global" (not tied to any specific entity
+  # in your database otherwise).
+  #
+  # This is useful for accounts that you don't want to model anything
+  # for via an ActiveRecord model (like "Cash").
   class GlobalAccount < ActiveRecord::Base
     self.table_name = :acts_as_account_global_accounts
 
